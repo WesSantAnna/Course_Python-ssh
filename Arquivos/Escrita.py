584c8564d9c753b0403fdfee3352eb1756c43669
Wesley SantAnna <wesley.santanna@semeq.com>
2025-05-08 11:54:05 -0300

    Colocando mais conteúdo

--- Arquivos/Escrita.py
+++ Arquivos/Escrita.py
@@ -10,7 +10,8 @@ print(type(arq))
 <_io.TextIOWrapper name='texto.txt' mode='r' encoding='cp1252'>
 <class '_io.TextIOWrapper'>
 
-# ALGUNS
+# -----------------------------------------------------------------
+# ALGUNS MÉTODOS
 
 arq = open('texto.txt', 'r+') # ABERTURA DE ARQUIVOS
 
@@ -39,11 +40,44 @@ with open('texto.txt') as f:
 
 print(f.close())
 
+# --------------------------------------------------------------
 # ABERTURA DE ARQUIVO
 
 with open('arquivo.txt', 'w') as arq:
     arq.write('Estamos comecançando uma nova etapa')
     arq.write('O passado não importa mais')
 
+    
+with open('Tarefas.txt', 'w') as taf:
+while True:
+    tarefa = input('Digite um tarefa ou digite sair: ')
+    if tarefa != 'sair':
+        taf.write(tarefa + '\n')
+    else:
+        break
+
+# --------------------------------------------------------------------
+# StringIO
+
+from io import StringIO as st
+
+mensagem = 'Colocando em prática utilização do módulo StringIO'
+
+arq = st(mensagem)
+
+arq.write('\nQue permite a manipulação de arquivo via memória')
+
+arq.seek(0)
+
+print(arq.read())
+
+# ---------------------------------------------------------------------
+
 
 """
+
+# MANIPULAÇÃO DE ARQUIVOS: NAVEGAÇÃO
+
+import os 
+
+print(os.getcwd)
