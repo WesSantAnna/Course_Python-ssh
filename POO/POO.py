ff087134c41af783d0ce23f8421e773aec2ef1b3
Wesley SantAnna <wesley.santanna@semeq.com>
2025-05-12 09:52:12 -0300

    Adicionando contéudo

--- POO/POO.py
+++ POO/POO.py
@@ -37,3 +37,34 @@ x.r, x.i
 
 """
 
+class Lampada:
+    def __init__(self, voltagem, cor):
+        self.voltagem = voltagem
+        self.cor = cor
+        self.ligada = False
+
+class ContaCorrente:
+    def __init__(self, numero, limite, saldo):
+        self.numero = numero
+        self.limite = limite
+        self.saldo = saldo
+class Produto:
+    def __init__(self, nome, descricao, valor):
+        self.nome = nome
+        self.descricao = descricao
+        self.valor = valor
+        
+class Usuario:
+    def __init__(self, nome, email, senha):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+
+class Acesso:
+    def __init__(self, email, senha):
+        self.email = email
+        self.senha = senha
+
+user = Acesso('wesley@semeq.com','1234')
+
+print(user.email)
--- olamundo.py
+++
@@ -1,9 +0,0 @@
-valores = [0,0,0]
-soma = 0
-square = [0,0,0]
-for i in range(len(valores)):
-    valores[i] = int(input("Digite um valor inteiro: "))
-    soma += valores[i]
-    square[i] = pow(valores[i],2)
-
-print(f'a soma dos valores são {soma} e o quadrado dos valores originais são: {square}')
