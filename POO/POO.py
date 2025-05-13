aed96d89541d2b0714df94be23da945fcca40b70
Wesley SantAnna <wesley.santanna@semeq.com>
2025-05-13 08:29:06 -0300

    Adicionando conteúdo

--- POO/POO.py
+++ POO/POO.py
@@ -35,36 +35,86 @@ x.r, x.i
 # SAÍDA
 # (3.0, -4.5)
 
-"""
+#---------------------------------------------------------
 
-class Lampada:
-    def __init__(self, voltagem, cor):
-        self.voltagem = voltagem
-        self.cor = cor
-        self.ligada = False
 
-class ContaCorrente:
-    def __init__(self, numero, limite, saldo):
-        self.numero = numero
-        self.limite = limite
-        self.saldo = saldo
-class Produto:
-    def __init__(self, nome, descricao, valor):
+# ------------------------------------------------------------
+
+class Cachorro:
+    tipo = 'canino'
+    def __init__(self, nome):
         self.nome = nome
-        self.descricao = descricao
-        self.valor = valor
-        
-class Usuario:
-    def __init__(self, nome, email, senha):
-        self.nome = nome
-        self.email = email
-        self.senha = senha
+
+d = Cachorro('Frido')
+e = Cachorro('Buddy')
+
+print(e.tipo)
+print(d.tipo)
+
+print(d.nome)
+print(e.nome)
+
+# ------------------------------------------------------------
 
 class Acesso:
     def __init__(self, email, senha):
         self.email = email
-        self.senha = senha
+        self.__senha = senha
 
 user = Acesso('wesley@semeq.com','1234')
 
 print(user.email)
+print(user.__senha)
+
+
+# -------------------------------------------------------------------
+
+class Produto:
+    imposto = 1.05
+    contador = 0
+
+    def __init__(self, nome, descrição, valor):
+        self.id = Produto.contador + 1
+        self.nome = nome
+        self.descrição = descrição
+        self.valor = (valor * Produto.imposto)
+        Produto.contador = self.id
+
+p1 = Produto('PS5', 'VideoGame', 3000)
+p2 = Produto('Xbox','VideoGame', 2500)
+
+print(p1.valor)
+print(p2.valor)
+
+print(p1.id)
+print(p2.id)
+
+"""
+
+
+
+class Lampada:
+    def __init__(self, voltagem, cor):
+        self.__voltagem = voltagem
+        self.__cor = cor
+        self.__ligada = False
+
+class ContaCorrente:
+    def __init__(self, numero, limite, saldo):
+        self.__numero = numero
+        self.__limite = limite
+        self.__saldo = saldo
+class Produto:
+    def __init__(self, nome, descricao, valor):
+        self.__nome = nome
+        self.__descricao = descricao
+        self.__valor = valor
+        
+class Usuario:
+    def __init__(self, nome, email, senha):
+        self.__nome = nome
+        self.__email = email
+        self.__senha = senha
+
+        def __correr__(self, metros):
+            
